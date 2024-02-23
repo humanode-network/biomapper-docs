6b6e1a7702dc588e5dbcd594d6f9348c96f52ad7
MOZGIII <mike-n@narod.ru>
2024-02-23 07:54:01 -0300

    Initial docs structure

---
+++ theme.config.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Humanode Biomapper</span>,
+  project: {
+    link: "https://github.com/humanode-network/biomapper-docs",
+  },
+  chat: {
+    link: "https://link.humanode.io/chat",
+  },
+  docsRepositoryBase:
+    "https://github.com/humanode-network/biomapper-docs/tree/master",
+  footer: {
+    text: "Humanode Biomapper Documentation",
+  },
+};
+
+export default config;
