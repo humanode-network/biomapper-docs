cdd83915698364ec47c1bf09008735da17e241fc
MOZGIII <mike-n@narod.ru>
2024-02-23 09:24:39 -0300

    Port the content from gitbook

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
+import logo from "./assets/logo.png";
 
 const config: DocsThemeConfig = {
-  logo: <span>Humanode Biomapper</span>,
+  logo: (
+    <>
+      <Image src={logo} height={32} width={32} alt="Humanode" />
+      <span>Humanode Biomapper</span>
+    </>
+  ),
   project: {
     link: "https://github.com/humanode-network/biomapper-docs",
   },
