c343658abdc71c8a6ebbee2392c1ea76dd9ffedf
MOZGIII <mike-n@narod.ru>
2025-01-25 08:10:54 -0300

    Update __meta files

---
+++ pages/_meta.ts
@@ -0,0 +1,28 @@
+export default {
+  index: {
+    title: "Overview",
+    theme: {
+      breadcrumb: false,
+    },
+  },
+  about: "About",
+  "contract-addresses": "Contract Addresses",
+  "user-flow": "User Flow",
+  generations: "Generations",
+  "---qwkejndq": {
+    type: "separator",
+  },
+  showcase: "Showcase",
+  "---naK0ahph": {
+    type: "separator",
+  },
+  integration: "Integration",
+  "---Paisahg1": {
+    type: "separator",
+  },
+  interfaces: {
+    title: "Interfaces ↗",
+    href: "https://link.humanode.io/docs/biomapper-sdk",
+    newWindow: true,
+  },
+};
