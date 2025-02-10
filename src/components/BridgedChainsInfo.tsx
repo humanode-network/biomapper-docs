691633f018646234286e8b298f7f5150c13e418e
MOZGIII <mike-n@narod.ru>
2025-02-10 19:13:42 -0300

    Document Cross-Chain Biomapper (#9)
    * Adjust user flow with the cross-chain bridging capabilities
    
    * Add the bridging page
    
    * Update biomapper sdk page
    
    * Add contract addresses at bridged chain
    
    * Implement BridgedChainsInfo component

---
+++ src/components/BridgedChainsInfo.tsx
@@ -0,0 +1,43 @@
+import { Code, Table } from "nextra/components";
+
+export type BridgedChainInfo = {
+  chainName: string;
+  addresses: {
+    bridgedBiomapper: string;
+  };
+};
+
+type Props = {
+  chains: BridgedChainInfo[];
+};
+
+const BridgedChainsInfo: React.FC<Props> = ({ chains }) => (
+  <Table className="x:not-first:mt-6 x:p-0">
+    <thead>
+      <Table.Tr>
+        <Table.Th>Chain</Table.Th>
+        <Table.Th>
+          <Code>BridgedBiomapper</Code> Address
+        </Table.Th>
+      </Table.Tr>
+    </thead>
+    <tbody>
+      {chains.length === 0 ? (
+        <Table.Tr>
+          <Table.Td colSpan={2}>Wait for announcements!</Table.Td>
+        </Table.Tr>
+      ) : (
+        chains.map((info) => (
+          <Table.Tr>
+            <Table.Td>{info.chainName}</Table.Td>
+            <Table.Td>
+              <Code>{info.addresses.bridgedBiomapper}</Code>
+            </Table.Td>
+          </Table.Tr>
+        ))
+      )}
+    </tbody>
+  </Table>
+);
+
+export default BridgedChainsInfo;
