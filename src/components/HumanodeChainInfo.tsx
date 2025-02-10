52d5e6b312669fc4fa651eef25ab0adfd6d25279
MOZGIII <mike-n@narod.ru>
2025-02-10 18:08:10 -0300

    Extract HumanodeChainInfo component (#18)
    Add HumanodeChainInfo component

---
+++ src/components/HumanodeChainInfo.tsx
@@ -0,0 +1,43 @@
+import { Code, Table } from "nextra/components";
+
+type Props = {
+  biomapperAddress: string;
+  biomapperLogAddress: string;
+  uiUrl: string;
+};
+
+const HumanodeChainInfo: React.FC<Props> = (props) => (
+  <>
+    <Table>
+      <thead>
+        <Table.Tr>
+          <Table.Td>Smart Contract</Table.Td>
+          <Table.Td>Address</Table.Td>
+        </Table.Tr>
+      </thead>
+      <tbody>
+        <Table.Tr>
+          <Table.Td>
+            <Code>Biomapper</Code>
+          </Table.Td>
+          <Table.Td>
+            <Code>{props.biomapperAddress}</Code>
+          </Table.Td>
+        </Table.Tr>
+        <Table.Tr>
+          <Table.Td>
+            <Code>BiomapperLog</Code>
+          </Table.Td>
+          <Table.Td>
+            <Code>{props.biomapperLogAddress}</Code>
+          </Table.Td>
+        </Table.Tr>
+      </tbody>
+    </Table>
+    <p className="x:not-first:mt-6 x:leading-7">
+      The UI URL: <Code>{props.uiUrl}</Code>.
+    </p>
+  </>
+);
+
+export default HumanodeChainInfo;
