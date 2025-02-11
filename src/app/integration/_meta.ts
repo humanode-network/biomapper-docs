ff661abbbfb64c8e26700fd6dd5c5ddf7097a3f9
MOZGIII <mike-n@narod.ru>
2025-02-10 22:30:59 -0300

    Drop the obsolete pages

--- src/app/integration/_meta.ts
+++ src/app/integration/_meta.ts
@@ -4,7 +4,4 @@ export default {
     title: "Solidity API",
     href: "https://link.humanode.io/docs/biomapper-sdk",
   },
-  "official-biomapper-http-api": "Official Biomapper HTTP API",
-  indexers: "Indexers",
-  bridges: "Bridges",
 };
