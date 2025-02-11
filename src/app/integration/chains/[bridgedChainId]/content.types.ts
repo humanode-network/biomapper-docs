692f2e50cfe2c2a33f59526b8ed66fcfdbd4330d
MOZGIII <mike-n@narod.ru>
2025-02-11 06:09:35 -0300

    Revert "Add the chains page (#33)"
    This reverts commit 133670c398128ec701e28d2f7df75e6dd62f7f4f.

--- src/app/integration/chains/[bridgedChainId]/content.types.ts
+++
@@ -1,6 +0,0 @@
-import { BridgedChainId, BridgedChains } from "../../../../data/stages.derived";
-
-export type Props<Id extends BridgedChainId> = {
-  bridgedChainId: Id;
-  bridgedChain: BridgedChains[Id];
-};
