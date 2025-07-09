8a82c49aabfba8a3c9ed59471398917521e71620
Aleksandr Neyasov <a.neyasov92@gmail.com>
2025-07-09 18:45:57 +0400

    Add Ancient8

--- src/data/stages.ts
+++ src/data/stages.ts
@@ -55,6 +55,9 @@ export const bridgedChains = {
   metis: {
     generalDisplayName: "Metis",
   },
+  ancient8: {
+    generalDisplayName: "Ancient8",
+  },
 } as const satisfies BridgedChainsShape;
 
 export const stages = {
@@ -157,6 +160,12 @@ export const stages = {
         displayName: "Metis Andromeda Mainnet",
         addresses: {
           bridgedBiomapper: "0x7Bc956A55ec813447142E1183974bE2e496Ee444",
+        },
+      },
+      ancient8: {
+        displayName: "Ancient8",
+        addresses: {
+          bridgedBiomapper: "0x95787e7eF7551c457ABA37982fFa9dE13558C6ed",
         },
       },
     },
@@ -267,6 +276,12 @@ export const stages = {
           bridgedBiomapper: "0x16F2a7AC67B6aC1E57dD5528A24b1fC689902Be2",
         },
       },
+      ancient8: {
+        displayName: "Ancient8 Testnet (Celestia)",
+        addresses: {
+          bridgedBiomapper: "0x16F2a7AC67B6aC1E57dD5528A24b1fC689902Be2",
+        },
+      },
     },
   },
 } as const satisfies StagesShape;
@@ -292,4 +307,5 @@ export const bridgedChainsDisplayOrder = defineBridgedChainIdsOrder([
   "unichain",
   "linea",
   "metis",
+  "ancient8",
 ]);
